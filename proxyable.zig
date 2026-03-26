38911493e83cb6f300be4015a80764b5c8f14b7f
gabewillen <gabewillen@gmail.com>
2026-03-26 08:59:52 -0500

    feat: enforce PascalCase DSL in Zig

--- proxyable.zig
+++ proxyable.zig
@@ -20,27 +20,16 @@ pub const Proxyable = struct {
     pub var exports: Registry = .{};
     pub var imports: Registry = .{};
 
-    pub const Export = export;
-    pub const ImportFrom = import_from;
-
-    pub fn @"export"(params: anytype) !@TypeOf(exported.create_exported_proxyable(params)) {
+    pub fn Export(params: anytype) !@TypeOf(exported.create_exported_proxyable(params)) {
         const proxy = try exported.create_exported_proxyable(params);
         try register(&exports, proxy);
         return proxy;
     }
 
-    pub fn Export(params: anytype) !@TypeOf(exported.create_exported_proxyable(params)) {
-        return try export(params);
-    }
-
-    pub fn import_from(params: anytype) !cursor.ProxyCursor {
+    pub fn ImportFrom(params: anytype) !cursor.ProxyCursor {
         const imported_proxy = try imported.create_imported_proxyable(params);
         try register(&imports, imported_proxy);
         return imported_proxy.root();
-    }
-
-    pub fn ImportFrom(params: anytype) !cursor.ProxyCursor {
-        return try import_from(params);
     }
 };
 
